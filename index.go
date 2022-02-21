0bd0e79aad02e5502c227d652a4a638a79c397db
ashley <altsaqifnugraha19@gmail.com>
2022-02-21 09:16:17 +0700

    Mengubah data pada index.go

--- index.go
+++ index.go
@@ -4,6 +4,8 @@ func main() {
 	fmt.Println("Hello, World!")
 	myName := "Al Tsaqif Nugraha Ahmad"
 	fmt.Println("my name is " + myName)
+	myAge := 20
+	fmt.Println("my name is " + myAge)
 	program := "Become a Master Golang Programme"
 	fmt.Println("my name is " + program)
 }
