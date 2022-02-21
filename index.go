61a550fdece6986ab382fe9739d401053a912506
ashley <altsaqifnugraha19@gmail.com>
2022-02-21 07:42:43 +0700

    Menambahkan nama program

--- index.go
+++ index.go
@@ -4,4 +4,6 @@ func main() {
 	fmt.Println("Hello, World!")
 	myName := "Al Tsaqif Nugraha Ahmad"
 	fmt.Println("my name is " + myName)
+	program := "Flutter"
+	fmt.Println("my name is " + program)
 }
